32b0a06ae1fa68df80fe9ad3e837f912e0ef6b67
taizilinger123 <837337164@qq.com>
2018-11-16 23:13:33 +0800

    2018-11-16

--- src/day04/ArrayListDemo4.java
+++ src/day04/ArrayListDemo4.java
@@ -17,7 +17,7 @@ public class ArrayListDemo4 {
 		/**
 		 * 获取回来时是以Object获取的，所以要造型
 		 */
-		String element = (String)list.get(1);
+		String element = (String)list.get(2);
 		System.out.println(element);//2
 		
         for (int i = 0; i < list.size(); i++) {
@@ -26,9 +26,28 @@ public class ArrayListDemo4 {
         
         /**
          * 将元素"2"替换为元素"二"
+         * 同样的，索引值不能超过size()-1
          */
           Object  old =  list.set(1, "二");
           System.out.println(list);
           System.out.println("被替换的元素："+old);     
+          
+          /**
+           * 在集合中第二个位置插入元素"一"
+           */
+          //[1,二,3]
+          list.add(1,"一");
+          System.out.println(list);//[1,一,二,3]
+          
+          /**
+           * 删除第3个元素
+           * 返回值为被删除的元素
+           */
+          Object  re =  list.remove(2);
+          System.out.println(re);
+          
+          //查看"3"在集合中的位置
+          int  index = list.indexOf("3");
+          System.out.println("3在集合中的位置："+index);
 	}
 }
